d186b41b6376351e14860365ff290258ef863464
devYeonjun <alcls0026@gmail.com>
2022-05-17 20:02:42 +1200

    Re-evaluate chil component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import './App.css';
-import Button from './components/UI/Button/Button'
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
-  const [showParagraph, setShowPragragh] = useState()
+	const [showParagraph, setShowPragragh] = useState();
 
-  const toggleParagraphHandler = () => {
-    setShowPragragh(prevShowParagraph => !prevShowParagraph)
-  }
-  return (
-    <div className="app">
-      {showParagraph && <p>This is New</p>}
-      <Button onClick={toggleParagraphHandler}>Show</Button>
-    </div>
-  );
+	console.log("APP RUNNING");
+
+	const toggleParagraphHandler = () => {
+		setShowPragragh((prevShowParagraph) => !prevShowParagraph);
+	};
+	return (
+		<div className="app">
+			<h1>Hello</h1>
+			<DemoOutput show={showParagraph} />
+			<Button onClick={toggleParagraphHandler}>Show</Button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MyParagraph = (props) => {
+	console.log("DEMO OUTPUT RUNNING");
+	return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
