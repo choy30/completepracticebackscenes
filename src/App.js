3b7dedebff0022d96ebc51e95c019fc5426b4344
devYeonjun <alcls0026@gmail.com>
2022-05-17 19:54:18 +1200

    Update component in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button'
 
 function App() {
+  const [showParagraph, setShowPragragh] = useState()
+
+  const toggleParagraphHandler = () => {
+    setShowPragragh(prevShowParagraph => !prevShowParagraph)
+  }
   return (
     <div className="app">
-      <h1>Hi there!</h1>
+      {showParagraph && <p>This is New</p>}
+      <Button onClick={toggleParagraphHandler}>Show</Button>
     </div>
   );
 }
